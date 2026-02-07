3c26b610c42af856c9c447db40988d4b7376f161
bluef1re <entertainment420xx@gmail.com>
2026-02-07 13:45:38 -0600

    able to call on each function individually

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -15,7 +15,7 @@ def get_file_content(working_directory, file_path):
                 file_content_string = f.read(MAX_CHARS)
                 if f.read(1):
                     file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
-                    return file_content_string
+            return file_content_string
         else:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     except Exception as e:
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from functions.prompts import system_prompt
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
 
 load_dotenv()
 api_key = os.environ.get('GEMINI_API_KEY')
@@ -28,11 +28,22 @@ def general_response(user_input, verbose=False):
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    #changed response.text --> response.function_calls
+    function_results = []
+
     function_calls = response.function_calls
     if function_calls:
         for function_call in function_calls:        
-            print(f"Calling function: {function_call.name}{function_call.args}.")
+            #print(f"Calling function: {function_call.name}{function_call.args}.")
+            function_call_results = call_function(function_call, verbose)
+            if not function_call_results.parts:
+                raise Exception("Error")
+            if function_call_results.parts[0].function_response == None:
+                raise Exception("Function response is None")
+            if function_call_results.parts[0].function_response.response == None:
+                raise Exception("Actual function result is None")
+            function_results.append(function_call_results.parts[0])
+            if verbose:
+                print(f"-> {function_call_results.parts[0].function_response.response}")
     else:
         print(response.text)
 
